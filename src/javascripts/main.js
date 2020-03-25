f4e043fc016a86c657bca6cc946c7e594b8d2e4c
Liza Star <lizadstar18@gmail.com>
2020-03-24 23:01:30 -0500

    successful auth

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,15 @@
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
+import authData from './helpers/data/authData/authData';
+import myNavBar from './components/myNavBar/myNavBar';
 import '../styles/main.scss';
 import auth from './components/auth/auth';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
   auth.loginButton();
+  myNavBar.logoutEvent();
 };
 
 init();
