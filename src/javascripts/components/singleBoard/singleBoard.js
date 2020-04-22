b6994ebf4c8e31b2e544c76bd668dede5ceef9e7
Liza Star <lizadstar18@gmail.com>
2020-04-21 21:30:13 -0500

    successfully added new pin

--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -2,6 +2,7 @@ import pinData from '../../helpers/data/pinData';
 import utils from '../../helpers/utils';
 import newPinComponent from '../newPin/newPin';
 
+// Function that allow you to delete a pin from a board
 
 const removePins = (e) => {
   const pinId = e.target.closest('.card').id;
@@ -13,11 +14,12 @@ const removePins = (e) => {
       // utils.printToDom('singleBoardView', '');
     }).catch((err) => console.error('could not delete pins', err));
 };
+// Function that creates a single new pin
 const makeNewPin = (e) => {
   e.preventDefault();
   const selectedBoard = $(e.target).closest('button')[0].dataset.id;
   const newPin = {
-    pinUrl: $('#pin-url').val(),
+    imageUrl: $('#pin-url').val(),
     boardId: selectedBoard,
   };
   pinData.addPins(newPin)
@@ -32,12 +34,14 @@ const makeNewPin = (e) => {
 const boardsDiv = $('#boards');
 const pinsDiv = $('#singleBoardView');
 
+// function that goes back to all boards
 const backToBoards = (e) => {
   e.preventDefault();
   boardsDiv.removeClass('hide');
   pinsDiv.addClass('hide');
 };
 
+// function that allows user to see a single board on the page with all pins for that board
 const buildSingleBoardView = (boardId) => {
   pinData.getPinsByBoardId(boardId)
     .then((singleBoard) => {
@@ -60,11 +64,12 @@ const buildSingleBoardView = (boardId) => {
     })
     .catch((err) => console.error('problem with single board', err));
 };
+// click event function that allows the user to see the selected board
 const viewBoardEvent = (e) => {
   const boardId = e.target.closest('.card').id;
   buildSingleBoardView(boardId);
 };
-
+// all click events for the single board page
 const singleBoardEvents = (boardId) => {
   $('body').on('click', '#back-button', backToBoards);
   $('body').on('click', '.delete-single-pin', boardId, removePins);
